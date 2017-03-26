84298d5adbec8bf7d52a483001a19276e3288db3
kosta2222 <muh_kosta@mail.ru>
2017-03-26 21:10:25 +0300

    platformaIdea

--- src/main/java/myModel.java
+++ src/main/java/myModel.java
@@ -2,30 +2,53 @@
 
 import com.kosta.mysql.ManagmentSystem;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Properties;
 
 /**
  * Created by papa on 24.03.2017.
  */
 public class myModel {
-ManagmentSystem ms;
-Connection connection;
-public myModel(){
-String str=System.getProperty("user.home");
-  ms=ManagmentSystem.getInstance(str+"\\file.dat");
-  connection=ms.getConn();
-}
-    public ResultSet getResult(String c) throws Exception{
-        ResultSet resultSet=null;
-        if(c.equals("out")){
-        Statement statement=connection.createStatement();
-        String s="select * from tovari_ceni";
-         resultSet=statement.executeQuery(s);}
-        return resultSet;
+    ManagmentSystem ms;
+    Connection connection;
+    private FileInputStream fin;
+    private Properties ht;
+    ResultSet resultSet;
 
+    public myModel() {
+        String str = System.getProperty("user.home");
+        ht = new Properties();
+        try {
+            fin = new FileInputStream(str + "\\A_platformaSql.dat");
+            if (fin != null) {
+                ht.load(fin);
+                fin.close();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        ms = ManagmentSystem.getInstance(str + "\\A_connect.dat");
+        connection = ms.getConn();
+    }
+
+    public ResultSet getResult(String c) throws Exception {
+    String _out=null;
+        if (c.equals("out")) {
+            _out=ht.getProperty("out");
+        }
+        Statement statement = connection.createStatement();
+        //String s = "select * from tovari_ceni";
+        resultSet = statement.executeQuery(_out);
+
+        return resultSet;
+}
 
 
     }
-}
+
